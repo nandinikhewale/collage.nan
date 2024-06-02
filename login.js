f602951bbcbd7acb3b1da21a7f45224bd3b9e671
Nandini Khewale <nandinikhewale99@gmail.com>
2024-06-02 12:54:54 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,11 +1,8 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
+const container = document.querySelector('.container');
+const signupButton = document.querySelector('.signup-section header');
+const loginButton = document.querySelector('.login-section header');
 
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
+loginButton.addEventListener('click', () => {container.classList.add('active');});
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
+
+signupButton.addEventListener('click', () => {container.classList.remove('active');});
